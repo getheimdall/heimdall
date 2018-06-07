7ed25b2ccd99136442277250e6e29c00ded3707c
marceloaguiarr <cabelo@gmail.com>
2018-06-07 18:54:38 -0300

    Added license

--- heimdall-core/src/test/java/br/com/conductor/heimdall/core/service/MiddlewareServiceTest.java
+++ heimdall-core/src/test/java/br/com/conductor/heimdall/core/service/MiddlewareServiceTest.java
@@ -1,5 +1,25 @@
 package br.com.conductor.heimdall.core.service;
 
+/*-
+ * =========================LICENSE_START==================================
+ * heimdall-core
+ * ========================================================================
+ * Copyright (C) 2018 Conductor Tecnologia SA
+ * ========================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ==========================LICENSE_END===================================
+ */
+
 import static org.junit.Assert.*;
 
 import java.io.IOException;
@@ -111,7 +131,6 @@ public class MiddlewareServiceTest {
 		
 		Property p = new Property();
 		middlewareProperty = p.getMiddlewares();
-		middlewareProperty.setRollbackLevels(0);
 		
 		middlewareDTO = new MiddlewareDTO();
 		middlewareDTO.setStatus(Status.ACTIVE);
