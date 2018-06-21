946833cf800a1d0a00322b6fe5f3a66f4032e943
marceloaguiarr <cabelo@gmail.com>
2018-06-21 15:03:07 -0300

    Created log entities

---
+++ heimdall-core/src/main/java/br/com/conductor/heimdall/core/entity/Trace.java
@@ -0,0 +1,56 @@
+package br.com.conductor.heimdall.core.entity;
+
+import java.util.List;
+
+import br.com.conductor.heimdall.core.trace.FilterDetail;
+import br.com.conductor.heimdall.core.trace.GeneralTrace;
+import br.com.conductor.heimdall.core.trace.RequestResponseParser;
+import br.com.conductor.heimdall.core.spec.StackTrace;
+import lombok.Data;
+
+@Data
+public class Trace {
+	
+    private String method;
+
+    private String url;
+
+    private Integer resultStatus;
+
+    private Long durationMillis;
+
+    private String insertedOnDate;
+
+    private Long apiId;
+
+    private String apiName;
+
+    private String app;
+
+    private String accessToken;
+
+    private String receivedFromAddress;
+
+    private String clientId;
+
+    private Long resourceId;
+
+    private String appDeveloper;
+    
+    private Long operationId;
+    
+    private RequestResponseParser request;
+    
+    private RequestResponseParser response;
+
+    private String pattern;
+    
+    private StackTrace stackTrace;
+
+    private List<GeneralTrace> traces;
+    
+    private List<FilterDetail> filters;
+
+    private String profile;
+
+}
