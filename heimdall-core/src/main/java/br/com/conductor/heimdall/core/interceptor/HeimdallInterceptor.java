ab33e8c19452d4584d42f69ef55cbe8fd684485f
dijalmasilva <dijalmacz@gmail.com>
2018-07-11 18:16:05 -0300

    Update package implementations the HeimdallInterceptor; Update IpsInterceptorService to filter when X-FORWARDED-FOR is null; Add parse for implementations the HeimdallInterceptor.

---
+++ heimdall-core/src/main/java/br/com/conductor/heimdall/core/interceptor/HeimdallInterceptor.java
@@ -0,0 +1,35 @@
+package br.com.conductor.heimdall.core.interceptor;
+
+/*-
+ * =========================LICENSE_START==================================
+ * heimdall-core
+ * ========================================================================
+ * Copyright (C) 2018 Conductor Tecnologia SA
+ * ========================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ==========================LICENSE_END===================================
+ */
+
+import br.com.conductor.heimdall.core.entity.Interceptor;
+import br.com.conductor.heimdall.core.enums.TypeExecutionPoint;
+
+import java.util.HashMap;
+
+public interface HeimdallInterceptor {
+
+    String getFile(TypeExecutionPoint typeExecutionPoint);
+
+    Object parseContent(String content);
+
+    HashMap<String, Object> buildParameters(Object objectCustom, HashMap<String, Object> parameters, Interceptor interceptor);
+}
