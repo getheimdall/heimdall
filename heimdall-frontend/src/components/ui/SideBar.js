c3b6f0c17ccb7860729f9eeac931695f47b85854
Dijalma Silva <dijalmacz@gmail.com>
2018-08-09 13:48:26 -0300

    Restricting actions with privileges in SideBar and NavBar

--- heimdall-frontend/src/components/ui/SideBar.js
+++ heimdall-frontend/src/components/ui/SideBar.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import Logo from './Logo'
 import {Layout, Menu} from 'antd'
 import SidebarLink from './SidebarLink'
+import {PrivilegeUtils} from "../../utils/PrivilegeUtils";
+import {privileges} from "../../constants/privileges-types";
 
 const {Sider} = Layout
 
@@ -24,37 +26,46 @@ class SideBar extends Component {
             <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
                 <Logo history={history} collapsed={this.state.collapsed}/>
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['apis']}>
+                    {PrivilegeUtils.verifyPrivileges([privileges.PRIVILEGE_READ_API]) &&
                     <Menu.Item key="apis" className="apis">
                         <SidebarLink to="/apis" label="APIs" history={history} icon="api"/>
                     </Menu.Item>
-
+                    }
+                    {PrivilegeUtils.verifyPrivileges([privileges.PRIVILEGE_READ_PLAN]) &&
                     <Menu.Item key="plans" className="plans">
                         <SidebarLink to="/plans" label="Plans" history={history} icon="profile"/>
                     </Menu.Item>
-
+                    }
+                    {PrivilegeUtils.verifyPrivileges([privileges.PRIVILEGE_READ_APP]) &&
                     <Menu.Item key="apps" className="apps">
                         <SidebarLink to="/apps" label="Apps" history={history} icon="appstore"/>
                     </Menu.Item>
-
+                    }
+                    {PrivilegeUtils.verifyPrivileges([privileges.PRIVILEGE_READ_ACCESSTOKEN]) &&
                     <Menu.Item key="access-tokens" className="accessToken">
                         <SidebarLink to="/tokens" label="Access Tokens" history={history} icon="key"/>
                     </Menu.Item>
-
+                    }
+                    {PrivilegeUtils.verifyPrivileges([privileges.PRIVILEGE_READ_ENVIRONMENT]) &&
                     <Menu.Item key="environments" className="environments">
                         <SidebarLink to="/environments" label="Environments" history={history} icon="codepen"/>
                     </Menu.Item>
-
+                    }
+                    {PrivilegeUtils.verifyPrivileges([privileges.PRIVILEGE_READ_DEVELOPER]) &&
                     <Menu.Item key="developers" className="developers">
                         <SidebarLink to="/developers" label="Developers" history={history} icon="code"/>
                     </Menu.Item>
-
+                    }
+                    {PrivilegeUtils.verifyPrivileges([privileges.PRIVILEGE_READ_USER]) &&
                     <Menu.Item key="users" className="users">
                         <SidebarLink to="/users" label="Users" history={history} icon="user"/>
                     </Menu.Item>
-
+                    }
+                    {PrivilegeUtils.verifyPrivileges([privileges.PRIVILEGE_READ_TRACES]) &&
                     <Menu.Item key="traces" className="traces">
                         <SidebarLink to="/traces" label="Traces" history={history} icon="sync"/>
                     </Menu.Item>
+                    }
                 </Menu>
             </Sider>
         )
