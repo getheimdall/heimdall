48f7962144c788008c6b54b45b74c674ebe0a5c2
Jonathan Camara <camara.js@gmail.com>
2018-08-20 12:57:18 -0300

    Adicionando license na exception

--- heimdall-middleware-spec/src/main/java/br/com/conductor/heimdall/middleware/exception/BeanValidationException.java
+++ heimdall-middleware-spec/src/main/java/br/com/conductor/heimdall/middleware/exception/BeanValidationException.java
@@ -1,7 +1,33 @@
 package br.com.conductor.heimdall.middleware.exception;
 
+/*-
+ * =========================LICENSE_START==================================
+ * heimdall-middleware-spec
+ * ========================================================================
+ * Copyright (C) 2018 Conductor Tecnologia SA
+ * ========================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ==========================LICENSE_END===================================
+ */
+
 import lombok.Getter;
 
+/**
+ * This class represents errors related to beans validations
+ * 
+ * @author <a href="https://github.com/jscamara">Jonathan Camara</a>
+ *
+ */
 @Getter
 public class BeanValidationException extends RuntimeException {
 
