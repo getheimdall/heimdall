e64990534f0bd05ce7d83ec328e9a565a519f02a
Felipe Brito <felipedebritolira@gmail.com>
2019-03-07 23:35:37 -0300

    Pull Request: Adjusted some exceptions that had code different from the thrown exception; Creating the unit test for the ExceptionMessage class; Adjusted how the raise () method throws the exception, previously used if/else conditions to identify the exception, now using the error code, the launch is done using a switch, based on error codes; Removed unused imports from classes: ContentTypeUtilsTest, AccessTokenServiceTest; Adjusted HTML line break tag of the GatewayApplication class documentation.

--- heimdall-core/src/main/java/br/com/conductor/heimdall/core/exception/ExceptionMessage.java
+++ heimdall-core/src/main/java/br/com/conductor/heimdall/core/exception/ExceptionMessage.java
@@ -25,6 +25,7 @@ import br.com.twsoftware.alfred.object.Objeto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -34,7 +35,8 @@ import static org.springframework.http.HttpStatus.*;
  * @author Filipe Germano
  * @author <a href="https://dijalmasilva.github.io" target="_blank">Dijalma Silva</a>
  * @author Marcelo Aguiar Rodrigues
- *
+ * @author <a href="https://github.com/felipe-brito" target="_blank" >Felipe Brito</a>
+ * 
  */
 @Slf4j
 public enum ExceptionMessage {
@@ -109,17 +111,19 @@ public enum ExceptionMessage {
 
     ENVIRONMENT_ATTACHED_TO_API(BAD_REQUEST.value(), "Environment attached to Api", BadRequestException.class),
 
-    ENVIRONMENT_INBOUND_DNS_PATTERN(BAD_REQUEST.value(), "Environment inbound URL has to follow the pattern http[s]://host.domain[:port] or www.host.domain[:port]", BadRequestException.class),PROVIDER_NOT_FOUND(BAD_REQUEST.value(), "Provider not found", BadRequestException.class),
+    ENVIRONMENT_INBOUND_DNS_PATTERN(BAD_REQUEST.value(), "Environment inbound URL has to follow the pattern http[s]://host.domain[:port] or www.host.domain[:port]", BadRequestException.class),
+    
+    PROVIDER_NOT_FOUND(BAD_REQUEST.value(), "Provider not found", BadRequestException.class),
      
     PROVIDER_USER_UNAUTHORIZED(UNAUTHORIZED.value(), "User provided unauthorized", UnauthorizedException.class),
      
     TOKEN_EXPIRED(UNAUTHORIZED.value(), "Token expired", UnauthorizedException.class),
 
-    TOKEN_INVALID(UNAUTHORIZED.value(), "Token not valid", ForbiddenException.class),
+    TOKEN_INVALID(FORBIDDEN.value(), "Token not valid", ForbiddenException.class),
 
     SIGNATURE_DOES_NOT_MATCH(UNAUTHORIZED.value(), "JWT signature does not match locally computed signature.", UnauthorizedException.class),
 
-    TOKEN_NOT_GENERATE(INTERNAL_SERVER_ERROR.value(), "Error to generate token", ForbiddenException.class),
+    TOKEN_NOT_GENERATE(FORBIDDEN.value(), "Error to generate token", ForbiddenException.class),
 
     CODE_NOT_FOUND(UNAUTHORIZED.value(), "Code already used to generate token or not defined", UnauthorizedException.class),
 
@@ -180,16 +184,16 @@ public enum ExceptionMessage {
     DEFAULT_PLAN_ALREADY_EXIST_TO_THIS_API(BAD_REQUEST.value(), "Default plan already exist to this Api", BadRequestException.class);
 
     @Getter
-    private Integer httpCode;
+    private final Integer httpCode;
 
     @Getter
     @Setter
     private String message;
 
-    private String defaultMessage;
+    private final String defaultMessage;
 
     @Getter
-    private Class<? extends HeimdallException> klass;
+    private final Class<? extends HeimdallException> klass;
 
     ExceptionMessage(int httpCode, String message, Class<? extends HeimdallException> klass) {
 
@@ -214,26 +218,23 @@ public enum ExceptionMessage {
 
         this.message = Objeto.isBlank(this.message) ? this.defaultMessage.replace("{}", "") : this.message;
 
-        if (this.badRequest()) {
-
-            throw new BadRequestException(this);
-        } else if (this.unauthorized()) {
-
-            throw new UnauthorizedException(this);
-        } else if (this.forbidden()) {
-
-            throw new ForbiddenException(this);
-        } else if (this.notFound()) {
-
-            throw new NotFoundException(this);
-        } else if (this.timeout()) {
-
-            throw new TimeoutException(this);
-        } else if (this.serverError()) {
-
-            throw new ServerErrorException(this);
+        switch(HttpStatus.valueOf(this.httpCode)){
+            case BAD_REQUEST:
+                throw new BadRequestException(this);
+            case UNAUTHORIZED:
+                throw new UnauthorizedException(this);
+            case FORBIDDEN:
+                throw new ForbiddenException(this);
+            case NOT_FOUND:
+                throw new NotFoundException(this);
+            case REQUEST_TIMEOUT:
+                throw new TimeoutException(this);
+            case INTERNAL_SERVER_ERROR:
+                throw new ServerErrorException(this);
+            default:
+                throw new ServerErrorException(this);
         }
-
+        
     }
 
     /**
@@ -269,55 +270,6 @@ public enum ExceptionMessage {
             }
         }
         raise();
-    }
-
-
-    /**
-     * Method responsible for validation of error codes with code 400.
-     */
-    private Boolean badRequest() {
-
-        return this.httpCode == BAD_REQUEST.value();
-    }
-
-    /**
-     * Method responsible for validation of error codes with code 401.
-     */
-    private Boolean unauthorized() {
-
-        return this.httpCode == UNAUTHORIZED.value();
-    }
-
-    /**
-     * Method responsible for validation of error codes with code 403.
-     */
-    private Boolean forbidden() {
-
-        return this.httpCode == FORBIDDEN.value();
-    }
-
-    /**
-     * Method responsible for validation of error codes with code 404.
-     */
-    private Boolean notFound() {
-
-        return this.httpCode == NOT_FOUND.value();
-    }
-
-    /**
-     * Method responsible for validation of error codes with code 408.
-     */
-    private Boolean timeout() {
-
-        return this.httpCode == REQUEST_TIMEOUT.value();
-    }
-
-    /**
-     * Method responsible for validation of error codes with code 500.
-     */
-    private Boolean serverError() {
-
-        return this.httpCode == INTERNAL_SERVER_ERROR.value();
     }
 
 }
