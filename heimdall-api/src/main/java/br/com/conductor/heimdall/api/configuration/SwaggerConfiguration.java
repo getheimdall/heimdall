8e071b643cfd1aea793dae959d004eae8dbe2197
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2019-03-29 11:34:08 -0300

    Removed unused imports, fields and classes

--- heimdall-api/src/main/java/br/com/conductor/heimdall/api/configuration/SwaggerConfiguration.java
+++ heimdall-api/src/main/java/br/com/conductor/heimdall/api/configuration/SwaggerConfiguration.java
@@ -35,11 +35,8 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.ApiKeyVehicle;
-import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
