bf6cffe94c5f1393609a0f9cf4552716adc88055
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2019-04-03 09:03:22 -0300

    Added license and removed unused file

--- heimdall-gateway/src/main/java/br/com/conductor/heimdall/gateway/router/EnvironmentInfoRepository.java
+++ heimdall-gateway/src/main/java/br/com/conductor/heimdall/gateway/router/EnvironmentInfoRepository.java
@@ -1,6 +1,24 @@
+/*-
+ * =========================LICENSE_START==================================
+ * heimdall-gateway
+ * ========================================================================
+ * Copyright (C) 2018 Conductor Tecnologia SA
+ * ========================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ==========================LICENSE_END===================================
+ */
 package br.com.conductor.heimdall.gateway.router;
 
-import br.com.conductor.heimdall.core.entity.Environment;
 import br.com.conductor.heimdall.core.util.ConstantsCache;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -42,7 +60,7 @@ public class EnvironmentInfoRepository {
             String getVariables = "select \"key\", value from variables where environment_id = " + environment.getId();
 
             Map variables = jdbcTemplate.query(getVariables, resultSetExtractor -> {
-                Map<String,String> result = new HashMap<>();
+                Map<String, String> result = new HashMap<>();
                 while (resultSetExtractor.next()) {
                     result.put(
                             resultSetExtractor.getString("key"),
