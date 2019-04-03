c660b84125bf37be715bf7cbbdb5bc627c71cd44
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2019-04-03 13:48:27 -0300

    Updated unit tests

--- heimdall-gateway/src/test/java/br/com/conductor/heimdall/gateway/filter/helper/HttpImplTest.java
+++ heimdall-gateway/src/test/java/br/com/conductor/heimdall/gateway/filter/helper/HttpImplTest.java
@@ -1,7 +1,7 @@
 package br.com.conductor.heimdall.gateway.filter.helper;
 
+import br.com.conductor.heimdall.gateway.failsafe.CircuitBreakerManager;
 import br.com.conductor.heimdall.middleware.spec.ApiResponse;
-import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,9 +11,8 @@ import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
+import java.util.concurrent.Callable;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
@@ -22,10 +21,10 @@ import static junit.framework.TestCase.assertNotNull;
 public class HttpImplTest {
      
      @InjectMocks
-     private HttpImpl subject;
-     
+     private HttpImpl subject = new HttpImpl(false, new CircuitBreakerManager());
+
      @Mock
-     private RestTemplate restTemplate;
+     private CircuitBreakerManager circuitBreakerManager;
      
      private ResponseEntity<String> responseEntity;
      
@@ -36,8 +35,8 @@ public class HttpImplTest {
      
      @Test
      public void sendGetWithNoParams() {
-          Mockito.when(restTemplate.getForEntity(Mockito.any(URI.class), Mockito.any(Class.class))).thenReturn(responseEntity);
-          
+          Mockito.when(circuitBreakerManager.failsafe(Mockito.any(Callable.class), Mockito.anyString())).thenReturn(responseEntity);
+
           ApiResponse apiResponse = subject.url("https://www.google.com/search")
                                            .sendGet();
           assertNotNull(apiResponse);
@@ -49,7 +48,7 @@ public class HttpImplTest {
      
      @Test
      public void sendGetWithInvalidParams() {
-          Mockito.when(restTemplate.getForEntity(Mockito.any(URI.class), Mockito.any(Class.class))).thenReturn(responseEntity);
+          Mockito.when(circuitBreakerManager.failsafe(Mockito.any(Callable.class), Mockito.anyString())).thenReturn(responseEntity);
           
           ApiResponse apiResponse = subject.url("https://www.google.com/search")
                                            .queryParam("search", null)
@@ -63,7 +62,7 @@ public class HttpImplTest {
      
      @Test
      public void setUrlThenAddQueryParam() {
-          Mockito.when(restTemplate.getForEntity(Mockito.any(URI.class), Mockito.any(Class.class))).thenReturn(responseEntity);
+          Mockito.when(circuitBreakerManager.failsafe(Mockito.any(Callable.class), Mockito.anyString())).thenReturn(responseEntity);
           
           ApiResponse apiResponse = subject.url("https://www.google.com/search")
                                            .queryParam("search", "Heimdall")
@@ -77,7 +76,7 @@ public class HttpImplTest {
      
      @Test
      public void addQueryParamThenSetUrl() {
-          Mockito.when(restTemplate.getForEntity(Mockito.any(URI.class), Mockito.any(Class.class))).thenReturn(responseEntity);
+          Mockito.when(circuitBreakerManager.failsafe(Mockito.any(Callable.class), Mockito.anyString())).thenReturn(responseEntity);
           
           ApiResponse apiResponse = subject.queryParam("search", "Heimdall")
                                            .url("https://www.google.com/search")
