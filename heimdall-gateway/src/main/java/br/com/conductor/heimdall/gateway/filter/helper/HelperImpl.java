2c1d59d01c44693403bbe17feea8301466431b97
Dijalma Silva <dijalmacz@gmail.com>
2019-04-23 17:09:48 -0300

    Added flag to toggle circuitBreaker in middlewares

--- heimdall-gateway/src/main/java/br/com/conductor/heimdall/gateway/filter/helper/HelperImpl.java
+++ heimdall-gateway/src/main/java/br/com/conductor/heimdall/gateway/filter/helper/HelperImpl.java
@@ -19,6 +19,7 @@
  */
 package br.com.conductor.heimdall.gateway.filter.helper;
 
+import br.com.conductor.heimdall.core.environment.Property;
 import br.com.conductor.heimdall.gateway.failsafe.CircuitBreakerManager;
 import br.com.conductor.heimdall.middleware.enums.DBType;
 import br.com.conductor.heimdall.middleware.spec.*;
@@ -39,6 +40,9 @@ public class HelperImpl implements Helper {
 	private ThreadLocal<byte[]> buffers;
 
 	private boolean enableHandler;
+
+	@Autowired
+	private Property property;
 
 	@Autowired
 	private CircuitBreakerManager circuitBreakerManager;
@@ -85,9 +89,7 @@ public class HelperImpl implements Helper {
 
 	@Override
 	public Http http() {
-
-		Http http = new HttpImpl(enableHandler, circuitBreakerManager);
-		return http;
+		return new HttpImpl(enableHandler, circuitBreakerManager, property.getFailsafe().isEnabled());
 	}
 
 	@Override
