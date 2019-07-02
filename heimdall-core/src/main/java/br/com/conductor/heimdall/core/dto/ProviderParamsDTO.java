4c5a00a5fa21cec44ed14a722cf36a33cc1572f6
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2019-07-02 13:36:53 -0300

    Updated license

--- heimdall-core/src/main/java/br/com/conductor/heimdall/core/dto/ProviderParamsDTO.java
+++ heimdall-core/src/main/java/br/com/conductor/heimdall/core/dto/ProviderParamsDTO.java
@@ -1,11 +1,6 @@
-package br.com.conductor.heimdall.core.dto;
-
-/*-
- * =========================LICENSE_START==================================
- * heimdall-core
- * ========================================================================
+/*
  * Copyright (C) 2018 Conductor Tecnologia SA
- * ========================================================================
+ *
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,8 +12,8 @@ package br.com.conductor.heimdall.core.dto;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ==========================LICENSE_END===================================
  */
+package br.com.conductor.heimdall.core.dto;
 
 import java.io.Serializable;
 import javax.validation.constraints.NotNull;
