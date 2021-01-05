c241c76b3b003da031c6d9e8a15cd1a87d79dee3
conductor\leticia.campelo.ext <UmaSenha_01>
2021-01-05 16:05:15 -0300

    Rest Template adjustment

--- heimdall-core/src/main/java/br/com/conductor/heimdall/core/service/ReCaptchaService.java
+++ heimdall-core/src/main/java/br/com/conductor/heimdall/core/service/ReCaptchaService.java
@@ -39,9 +39,12 @@ import static br.com.conductor.heimdall.core.exception.ExceptionMessage.UNAVAILA
 @Service
 public class ReCaptchaService {
 
+     private RestTemplate restTemplate;
+
+
      public GoogleCaptchaDTO validateCaptcha(String response, String secret){
 
-          RestTemplate restTemplate = new RestTemplate();
+          initRestTemplate();
 
           String url =  "https://www.google.com/recaptcha/api/siteverify?response=" + response + "&secret=" + secret;
           GoogleCaptchaDTO googleCaptchaValidation =  restTemplate.getForObject(url, GoogleCaptchaDTO.class);
@@ -50,6 +53,12 @@ public class ReCaptchaService {
           HeimdallException.checkThrow(!googleCaptchaValidation.isSuccess(), INVALID_CAPTCHA_VALIDATION);
 
          return googleCaptchaValidation;
+     }
+
+     private void initRestTemplate(){
+          if (this.restTemplate == null){
+               restTemplate = new RestTemplate();
+          }
      }
 
 }
